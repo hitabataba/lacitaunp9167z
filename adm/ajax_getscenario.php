ceb38a33fc8bc24f1dc72175f0fd5ac0beace5e2
hitabataba <hitabataba@gmail.com>
2018-11-28 22:17:42 +0900

    html form

---
+++ adm/ajax_getscenario.php
@@ -0,0 +1,28 @@
+<?php
+header('Content-type: application/json; charset=utf-8');
+
+
+define('TABLE_NAME_PROGRESS', 'user_progress');
+define('TABLE_NAME_SCENARIO', 'scenario_data');
+
+require_once(__DIR__.'/../lib/dbconnect.php');
+
+
+/*
+  Create Database Connection
+ */
+try{
+    $dbh = dbConnection::getConnection();
+}catch(PDOException $e){
+    print('Connection failed:'.$e->getMessage());
+    die();
+}
+
+$export_sql = "SELECT * FROM ".TABLE_NAME_SCENARIO ;
+// query database
+$stmt = $dbh->query($export_sql);
+
+echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
+// close database connection
+$dbh = null;
+?>
