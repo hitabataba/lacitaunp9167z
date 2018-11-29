86962a84314248d8da0fb2569f298bca4a3020b8
hitabataba <hitabataba@gmail.com>
2018-11-29 14:27:44 +0900

    debug

--- index.php
+++ index.php
@@ -28,7 +28,6 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET
 // LINE Messaging APIがリクエストに付与した署名を取得
 $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 
-/*
 // 署名が正当かチェック。正当であればリクエストをパースし配列へ
 // 不正であれば例外の内容を出力
 try {
@@ -42,7 +41,6 @@ try {
 } catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
   error_log('parseEventRequest failed. InvalidEventRequestException => '.var_export($e, true));
 }
-*/
 
 //シナリオデータ
 $text = array();
