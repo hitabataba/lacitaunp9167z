37ce83ad2c437f44b45399dde57297f09e8424da
hitabataba <hitabataba@gmail.com>
2019-08-05 15:41:58 +0900

    シアリオフォーム

---
+++ adm/ajax_getscenario.php
@@ -0,0 +1,33 @@
+<?php
+header('Content-type: application/json; charset=utf-8');
+
+setlocale(LC_ALL, 'ja_JP.UTF-8');
+define('TABLE_NAME_PROGRESS', 'user_progress');
+define('TABLE_NAME_SCENARIO', 'scenario_data');
+define('TABLE_NAME_SCENARIO_ORDER', 'scenario_order');
+
+require_once(__DIR__.'/../lib/dbconnect.php');
+
+
+/*
+  Create Database Connection
+ */
+try{
+    $dbh = dbConnection::getConnection();
+}catch(PDOException $e){
+    print('Connection failed:'.$e->getMessage());
+    die();
+}
+
+$export_sql =
+"SELECT * FROM ".TABLE_NAME_SCENARIO ." as SC".
+"INNER JOIN ".TABLE_NAME_SCENARIO_ORDER ." as SC_ORDER".
+"ON SC.label = SC_ORDER.label";
+
+// query database
+$stmt = $dbh->query($export_sql);
+
+echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
+// close database connection
+$dbh = null;
+?>
